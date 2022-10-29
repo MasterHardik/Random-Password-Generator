748bc01ed00fb586a656559b0a875a25d4b69266
unknown <hardikchouhan1901@gmail.com>
2022-10-29 20:18:07 +0530

    Added pwd-gen main files

---
+++ REDME.md
@@ -0,0 +1,6 @@
+## Random Password Generator : JS Mini Project
+
+# Generate instant random password to use anywhere
+
+=> Generate custom password from range [2,30].
+=> Customize it with the use UpperCase , LowerCase , SpecialCharacter or Numbers.
---
+++ pwd-gen.js
@@ -0,0 +1,66 @@
+const upperSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const lowerSet = "abcdefghijklmnopqrstuvwxyzs"
+const numberSet = "0123456789"
+const specialSet = "~!@#$%^&*()_+=./;:'][{}|-?"
+
+var getRandomData = (DataSet) =>
+{
+    return DataSet[Math.floor(Math.random()*DataSet.length)]
+}
+
+//imortant selectors
+const PswdBox = document.getElementById("pswd-box"); 
+const LenInput    = document.getElementById("total-char");
+const UpperInput  = document.getElementById("upper-case");
+const LowerInput  = document.getElementById("lower-case");
+const NumberInput = document.getElementById("numbers");
+const SymbolInput = document.getElementById("symbols");
+const GenrateBTN = document.getElementById("gen-btn");
+
+const generatePassword = (password = "") =>
+{
+    if (UpperInput.checked)
+    {
+        password += getRandomData(upperSet);
+    }
+    if (LowerInput.checked)
+    {
+        password += getRandomData(lowerSet);
+    }
+    if (NumberInput.checked)
+    {
+        password += getRandomData(numberSet);
+    }
+    if (SymbolInput.checked)
+    {
+        password += getRandomData(specialSet);
+    }
+    if (password.length < LenInput.value)
+    {
+        return generatePassword(password);
+    }
+    return password.substring(0,LenInput.value);
+}
+
+document.getElementById("gen-btn").addEventListener(
+    "click",
+    function ()
+    {
+        PswdBox.innerHTML = generatePassword();    ;
+    }
+)
+
+
+
+    // RanPwd = password;
+    // if (password.length <= LenInput.value)
+    // {
+    //     return generatePassword(password);
+    // }// due to this we need to trim as it should owe length
+    // console.log(truncateString(password.LenInput.value));
+// PswdBox.innerHTML = getRandomData(specialSet);
+// console.log(getRandomData(specialSet));
+// console.log(getRandomData(lowerSet));
+// console.log(getRandomData(upperSet));
+// console.log(getRandomData(numberSet));
+// generatePassword();
